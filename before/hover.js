2d5fc060450c4e76f475668a88f3b48d9891edbc
808-cmd <79201337+JonMonday@users.noreply.github.com>
2022-10-22 10:58:47 +0000

    Merge branch 'main' of https://github.com/JonMonday/plane-riot-site

--- before/hover.js
+++
@@ -1,9 +0,0 @@
-function enter(card){
-    document.getElementById(card).style.display = 'block';
-    document.getElementById('ban-main').style.display = 'none';
-}
-
-function leave(card){
-    document.getElementById(card).style.display = 'none';
-    document.getElementById('ban-main').style.display = 'flex';
-}
